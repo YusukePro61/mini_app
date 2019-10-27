f6cdfed07078e9083362873b0f293b769b4c6da1
YusukePro61 <learning630@gmail.com>
2019-10-27 23:56:19 +0900

    create_article_table

--- db/migrate/20191026162558_create_articles.rb
+++ db/migrate/20191026162558_create_articles.rb
@@ -2,7 +2,11 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
 
-      t.timestamps
+      t.timestamps null: false
+      t.string :title, null: false
+      t.string :image
+      t.string :detail
+      t.references :user, foreign_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_135241) do
+ActiveRecord::Schema.define(version: 2019_10_26_162558) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title", null: false
+    t.string "image"
+    t.string "detail"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_10_23_135241) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
